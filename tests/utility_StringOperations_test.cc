7b797cd0a92948cb52d0445da88ae7d4b0f629b4
Harrison <hmatthews070@gmail.com>
2024-11-08 13:35:05 -0600

    Back to the correct test case from previous commit

--- tests/utility_StringOperations_test.cc
+++ tests/utility_StringOperations_test.cc
@@ -5,5 +5,5 @@ TEST(Utility_StringOperations, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
-  EXPECT_EQ(7 * 5, 42);
+  EXPECT_EQ(7 * 6, 42);
 }
