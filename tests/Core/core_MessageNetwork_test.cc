5e3689540aab70113895281e19639a3b798e53e4
H-Matthews <hmatthews070@gmail.com>
2025-01-13 23:00:59 -0600

    Initialize Network logger for TestFixture

--- tests/Core/core_MessageNetwork_test.cc
+++ tests/Core/core_MessageNetwork_test.cc
@@ -149,6 +149,7 @@ class CoreMessageNetworkTest : public ::testing::Test
         
         void SetUp() override 
         {
+            testNetwork.initializeLogger();
             publisher = std::make_unique< TestPublisher >(&testNetwork);
             subscriberOne = std::make_unique< TestSubscriberOne >(&testNetwork);
         }
